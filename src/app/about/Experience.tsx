05e72be194b9ef941083e3df16524a90ab6cdac1
Muehehe1 <darkrazo400@gmail.com>
2024-08-23 15:35:26 +0800

    Fixed some bugs

--- src/app/about/Experience.tsx
+++ src/app/about/Experience.tsx
@@ -67,7 +67,7 @@ const Experience: React.FC = () => {
     return (
         <>
             <motion.div variants={textVariant(0)}>
-                <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">The milestones I have reached in my journey.</p>
+                <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">{'The milestones I have reached in my journey'}.</p>
                 <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Work Experiences</h2>
             </motion.div>
             <div className="mt-20 flex flex-col">
